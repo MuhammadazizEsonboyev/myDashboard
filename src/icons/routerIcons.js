6b2e7f53064ec5c3b8a8dfa46d0c4383545c85f9
MuhammadazizEsonboyev <esonboyev03@bk.ru>
2022-12-07 12:57:40 +0500

    fix

--- src/icons/routerIcons.js
+++ src/icons/routerIcons.js
@@ -3,11 +3,13 @@ import HomeIcon from '@mui/icons-material/Home';
 import CategoryIcon from '@mui/icons-material/Category';
 import ClassIcon from '@mui/icons-material/Class';
 
-export default function (iconName) {
+const int = (iconName) =>   {
     const icons = {
         "/": <HomeIcon  sx={{color: "#9FA2B4"}}/>,
         "/product": <CategoryIcon  sx={{color: "#9FA2B4"}}/>,
         "/product-list": <ClassIcon  sx={{color: "#9FA2B4"}}/>,
     };
     return icons[iconName];
+
 }
+export default int
